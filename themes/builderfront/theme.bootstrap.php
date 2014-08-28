50f3f1d32a0c33918d21f950b6a1697f6b6850b1
Payam Naderi <naderi.payam@gmail.com>
2014-08-28 11:25:04 +0430

    init ui management template

--- themes/builderfront/theme.bootstrap.php
+++ themes/builderfront/theme.bootstrap.php
@@ -2,6 +2,7 @@
 /**
  * @var $this \yimaTheme\Theme\Theme
  */
+use Zend\Stdlib\ArrayUtils;
 
 /**
  * Theme Resolver Run This Bootstrap And
@@ -13,3 +14,27 @@
 $this->isFinal = false;
 
 $this->setTemplate('partial/builderfront/management-template');
+
+// ---- Register Assets File Into AssetManager Service --------------------------------------------------------------------------------------------\
+/*
+ * These Config must merged to application config at last
+ * : see below
+ */
+$ovverideConfigs = array(
+    'asset_manager' => array(
+        'resolver_configs' => array(
+            'paths' => array(
+                __DIR__.DS.'www',
+            ),
+        ),
+    ),
+);
+
+// ---- Merge new config to application merged config ---------------------------------------------------------------------------------------------\
+$mergedConf = $this->getServiceLocator()->get('Config');
+$config     = ArrayUtils::merge($mergedConf, $ovverideConfigs);
+
+$this->getServiceLocator()
+    ->setAllowOverride(true)
+    ->setService('config', $config)
+    ->setAllowOverride(false);
