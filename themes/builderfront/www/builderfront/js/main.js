46c05cfc5caa7f825050e65ccb16c08196eda32b
Payam Naderi <naderi.payam@gmail.com>
2014-08-28 16:10:32 +0430

    init implement drag widgets & drop onto area feature

---
+++ themes/builderfront/www/builderfront/js/main.js
@@ -0,0 +1,86 @@
+(function($)
+{
+    /**
+     * Areas Place Holder
+     * @type {string}
+     */
+    var AREA_PLACEHOLDER  = '.builderfront_area_holder';
+
+    var DRAGGABLE_ELEMENT = '.builderfront_element_dragable';
+
+    $(window).load(function() {
+        makeSortable();
+        makeDraggable();
+    });
+
+    function makeSortable()
+    {
+        var sortables = $(AREA_PLACEHOLDER);
+
+        sortables.sortable({
+            accept: DRAGGABLE_ELEMENT,
+            greedy: true, // any parent droppables will not receive the element. The drop event event.target can be checked to see which droppable received the draggable element.
+            scope: "widgetizer",
+            // Triggered when the droppable is created
+            create: function(event, ui){
+                var placeHolder = event.target;
+                $(placeHolder).find('.builderfront_start_empty_area').removeClass('ui-sortable-handle');
+            },
+            // Triggered when an accepted draggable starts dragging
+            activate: function(event, ui){
+                $(AREA_PLACEHOLDER).addClass("builderfront_area_holder_hover");
+            },
+            // Triggered when an accepted draggable stops dragging
+            deactivate: function(event, ui) {
+                $(AREA_PLACEHOLDER).removeClass("builderfront_area_holder_hover");
+            },
+            // Triggered when an accepted draggable is dropped on the droppable
+            drop: function(event, ui){
+            },
+            // Triggered when an accepted draggable is dragged out of the droppable
+            out: function(event, ui){
+                var placeHolder = event.target;
+                if( $(placeHolder).find('.ui-sortable-handle').size() == 0 ) {
+                    // Bring Drop Message, We didn`t have any widget inserted
+                    $(placeHolder).find('.builderfront_start_empty_area').fadeIn();
+                }
+            },
+            // Triggered when an accepted draggable is dragged over the droppable
+            over: function(event, ui){
+                // change dragable
+                var $draggable = ui.helper;
+                $draggable.html('Drop Me Here');
+                // clear empty drop message
+                var placeHolder = event.target;
+                $(placeHolder).find('.builderfront_start_empty_area').fadeOut();
+            }
+        });
+    }
+
+    function makeDraggable()
+    {
+        $(DRAGGABLE_ELEMENT).each(function(){
+            $(this).draggable({
+                // addClasses: false,
+                appendTo: 'body',
+                // cancel: ".title", // Prevents from dragging
+                connectToSortable: AREA_PLACEHOLDER,
+                // cursor: "crosshair",
+                delay: 100,
+                helper: function() {
+                    return $('<div style="height: 80px; width: 100px; background: #F9FAFA; box-shadow: 5px 5px 1px rgba(0,0,0,0.1); text-align: center; line-height: 100px; font-size: 10px; color: #16A085">Drop Me To Area</div>');
+                },
+                iframeFix: true,
+                opacity: 0.55,
+                refreshPositions: true,
+                revert: 'invalid',
+                revertDuration: 300,
+                scope: "widgetizer", // A draggable with the same scope value as a droppable will be accepted by the droppable
+                snap: true,
+                snapMode: "inner",
+                snapTolerance: 30
+            });
+        });
+    }
+
+})(jQuery);
