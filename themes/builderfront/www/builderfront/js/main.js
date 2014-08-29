32e67f1e81065a3674969e889070b89ff274c5be
Payam Naderi <naderi.payam@gmail.com>
2014-08-29 15:37:16 +0430

    init widget settings buttons

--- themes/builderfront/www/builderfront/js/main.js
+++ themes/builderfront/www/builderfront/js/main.js
@@ -9,63 +9,9 @@
     var DRAGGABLE_ELEMENT = '.builderfront_element_dragable';
 
     $(window).load(function() {
-        //makeDropable();
         makeDraggable();
         makeSortable();
     });
-
-    function makeDropable()
-    {
-        var sortables = $(AREA_PLACEHOLDER);
-
-        sortables.droppable({
-            accept: DRAGGABLE_ELEMENT,
-            greedy: false, // any parent droppables will not receive the element. The drop event event.target can be checked to see which droppable received the draggable element.
-            scope: "widgetizer",
-            // Triggered when the droppable is created
-            create: function(event, ui){
-                var $placeHolder = event.target;
-                $($placeHolder).find('.builderfront_start_empty_area').removeClass('ui-sortable-handle');
-            },
-            // Triggered when an accepted draggable starts dragging
-            activate: function(event, ui){
-                $(AREA_PLACEHOLDER).addClass("builderfront_area_holder_hover");
-            },
-            // Triggered when an accepted draggable stops dragging
-            deactivate: function(event, ui) {
-                $(AREA_PLACEHOLDER).removeClass("builderfront_area_holder_hover");
-            },
-            // Triggered when an accepted draggable is dropped on the droppable
-            drop: function(event, ui){
-                var $placeHolder = event.target;
-                var $draggable   = ui.draggable;
-
-                // fade out empty message
-                $($placeHolder).find('.builderfront_start_empty_area').fadeOut();
-
-                var $widget = $draggable.attr('id');
-                $($placeHolder).widgetizerDrop({widget: $widget});
-            },
-            // Triggered when an accepted draggable is dragged out of the droppable
-            out: function(event, ui){
-                var $placeHolder = event.target;
-                if( $($placeHolder).find('.ui-sortable-handle').size() == 0 ) {
-                    // Bring Drop Message, We didn`t have any widget inserted
-                    $($placeHolder).find('.builderfront_start_empty_area').fadeIn();
-                }
-            },
-            // Triggered when an accepted draggable is dragged over the droppable
-            over: function(event, ui){
-                // change dragable to drop here
-                var $draggable = ui.helper;
-                $draggable.html('Drop Me Here');
-
-                // clear empty drop message
-                var $placeHolder = event.target;
-                //$($placeHolder).find('.builderfront_start_empty_area').fadeOut();
-            }
-        });
-    }
 
     function makeDraggable()
     {
@@ -111,7 +57,7 @@
             //connectWith: AREA_PLACEHOLDER, // move widgets to another places
             cursor: "move",
             // Specifies which items inside the element should be sortable
-            items: ".builderfront_widget_holder",
+            items: ".ui-sortable-handle",
             create: function(event, ui){
                 var $placeholder = $(event.target);
                 // we don't want empty message as an item
@@ -140,6 +86,7 @@
             },
             out: function(event, ui){
                 _downlightPlaceholder($(event.target));
+                _revertHelperDropMe(ui.helper);
             }
         });
     }
@@ -213,7 +160,7 @@
      */
     function _revertHelperDropMe($helper)
     {
-        $helper.html('Drag Me to an Area');
+        //$helper.html('Drag Me to an Area');
     }
 
     /**
@@ -233,11 +180,13 @@
 
         options.params.html_content = $('.jumbotron').html();
 
-        // Append Widget Holder
+        // Append Widget Holder >>>>>>>>
         var $widgetHolder = $(this);
-        $widgetHolder.attr('class', 'builderfront_widget_holder builderfront_loading_content');
+        $widgetHolder.attr('class', 'builderfront_widget_holder ui-sortable-handle builderfront_loading_content');
         $widgetHolder.attr('rel-data', options.widget);
         $widgetHolder.html('');
+        // <<<<<<<<<<
+
 
         // widgetizer-[widgetNameHere]
         var $widgetArr = options.widget.split('-');
@@ -246,6 +195,10 @@
         options.callback = function(element, response) {
             element.removeClass('builderfront_loading_content');
             $.fn.widgetator.defaultCallback(element, response);
+
+            //add widget buttons
+            delButton = $('<button type="button" class="builderfront_settings builderfront_btn btn-danger"><span class="fa fa-trash-o  white"></span> remove</button>');
+            element.append(delButton);
         };
 
         $($widgetHolder).widgetator(options);
